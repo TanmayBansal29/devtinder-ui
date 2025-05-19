98a136bb9b3b6c492ad8b882968e3047f4f6f008
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-19 16:26:32 +0530

    Testing of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
-      <NavBar />
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>Base Page</div>}/>
+          <Route path="/login" element={<div>Login Page</div>} />
+          <Route path="/test" element={<div>Test Page</div>} />
+        </Routes>
+      </BrowserRouter>
+      {/* <NavBar />
+      <h1 className="text-3xl font-bold underline">Hello World</h1> */}
     </>
   )
 }
